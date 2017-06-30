98e6d2f637b102b146743d533af4cef8d273b715
Mike Risse <rissem@gmail.com>
2017-06-29 17:45:18 -0700

    send lines of lyrics

--- api/src/quantizeSong.js
+++ api/src/quantizeSong.js
@@ -6,22 +6,28 @@ const quantizeSong = function (song) {
   lyrics = song.data.lyrics.slice()
   const qChords = []
   const qLyrics = []
+  const qLines = []
+  let currentLine = null
   for (let i = 1; i < beats.length - 1; i++) {
     let midbeat = (beats[i].time + beats[i - 1].time) / 2
     while (chords.length > 0 && chords[0].time < midbeat) {
       qChords.push({beat: i, chord: chords.shift().chord})
     }
-
     // breaks when first lyric is before first beat..
     while (lyrics.length > 0 && lyrics[0].time > beats[i - 1].time &&
         lyrics[0].time < beats[i].time) {
       let lyric = lyrics.shift()
       let position = (lyric.time - beats[i - 1].time) / (beats[i].time - beats[i - 1].time)
+      if (lyric.startOfLine){
+        if (currentLine){qLines.push(currentLine)}
+        currentLine = {beat: i + position, lyrics: []}
+      }
+      currentLine.lyrics.push(lyric.lyric)
       qLyrics.push(({beat: i + position, lyric: lyric.lyric}))
     }
   }
   delete song.data
-  return _.extend(song, {chords: qChords, lyrics: qLyrics})
+  return _.extend(song, {chords: qChords, lyrics: qLyrics, lines: qLines})
 }
 
 module.exports = quantizeSong
